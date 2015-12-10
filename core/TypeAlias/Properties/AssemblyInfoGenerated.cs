6e280cd0b9b4bf3433d14027c4677573a30ac3d5
Esun Kim <veblush@gmail.com>
2015-12-10 13:52:06 +0900

    Update NetLegacySupport 1.1.0

--- core/TypeAlias/Properties/AssemblyInfoGenerated.cs
+++ core/TypeAlias/Properties/AssemblyInfoGenerated.cs
@@ -2,8 +2,8 @@
 using System.Reflection;
 
 [assembly: AssemblyVersionAttribute("1.0.0")]
-[assembly: AssemblyFileVersionAttribute("1.0.2")]
-[assembly: AssemblyInformationalVersionAttribute("1.0.2")]
+[assembly: AssemblyFileVersionAttribute("1.1.0")]
+[assembly: AssemblyInformationalVersionAttribute("1.1.0")]
 namespace System {
     internal static class AssemblyVersionInformation {
         internal const string Version = "1.0.0";
--- core/TypeAlias/TypeAlias.Release.md
+++ core/TypeAlias/TypeAlias.Release.md
@@ -1,4 +1,4 @@
-## 1.0.3 (Released 2015/12/10)
+## 1.1.0 (Released 2015/12/10)
 
 * Set AssemblyVersion to 1.0.0 to make a compatible dll.
 
