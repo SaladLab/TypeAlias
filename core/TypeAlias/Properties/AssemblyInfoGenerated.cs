13f5c7e720aada2020a5c3f39e9485637a914ffe
Esun Kim <veblush@gmail.com>
2016-02-29 17:15:01 +0900

    Set version to 1.1.2

--- core/TypeAlias/Properties/AssemblyInfoGenerated.cs
+++ core/TypeAlias/Properties/AssemblyInfoGenerated.cs
@@ -2,8 +2,8 @@
 using System.Reflection;
 
 [assembly: AssemblyVersionAttribute("1.0.0")]
-[assembly: AssemblyFileVersionAttribute("1.1.1")]
-[assembly: AssemblyInformationalVersionAttribute("1.1.1")]
+[assembly: AssemblyFileVersionAttribute("1.1.2")]
+[assembly: AssemblyInformationalVersionAttribute("1.1.2")]
 namespace System {
     internal static class AssemblyVersionInformation {
         internal const string Version = "1.0.0";
